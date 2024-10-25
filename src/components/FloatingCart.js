0784b792429193cbee3a72c562308bac23e35f57
RaazDk1992 <dhakal1992raaz@gmail.com>
2024-10-25 08:19:42 +0545

    payment method integration init

--- src/components/FloatingCart.js
+++ src/components/FloatingCart.js
@@ -1,8 +1,30 @@
-import { useState } from "react";
-import'./../assets/styles/FloatingCart.css';
-export default function FloatingCart(){
-    const[cartexpanded,setCartExpanded] = useState(false);
-    return(<div id="floating-cart">
-        <button className="fa fa-shopping-cart fa-2x ecom-cart-icon"></button>
-    </div>);
+import { useContext, useState } from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../assets/styles/FloatingCart.css';
+import { CartContext } from "../provider/CartDataProvider";
+
+export default function FloatingCart() {
+    const [cartexpanded, setCartExpanded] = useState(false);
+    const {cartItems} = useContext(CartContext);    
+    return (
+        <div id="floating-cart">
+            <Dropdown>
+                <Dropdown.Toggle as="div" className="icon-toggle">
+                    <button className="fa fa-shopping-cart fa-2x ecom-cart-icon"></button>
+                    
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+
+                  {cartItems.length >0?(cartItems
+                    .map(items=>( <Dropdown.Item as="button">Action</Dropdown.Item>)))
+                    :(<Dropdown.Item as="button">You have not added any item</Dropdown.Item>)}
+                   
+                   
+                </Dropdown.Menu>
+            </Dropdown>
+           
+        </div>
+    );
 }
