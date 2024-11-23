a42c7a33522fe16db1558aefed990bf92b4fe4f7
RaazDk1992 <dhakal1992raaz@gmail.com>
2024-11-23 12:02:16 +0545

    carousel integration (embla carousel)

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,16 +1,36 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import api from "../provider/api";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 export default function Slider(){
 
     const[slideItems,setSlideItems] = useState([]);
+    const[loading,setLoading] = useState(true);
+
+    const autoplayOptions = { delay: 4000 }; // 4-second delay
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay(autoplayOptions)]);
+
+    const onSelect = useCallback(() => {
+        if (!emblaApi) return;
+        const selectedIndex = emblaApi.selectedScrollSnap();
+        console.log(`Current slide: ${selectedIndex}`);
+      }, [emblaApi]);
+
+      useEffect(() => {
+        if (!emblaApi) return;
+        emblaApi.on("select", onSelect);
+      }, [emblaApi, onSelect]);
+
     const fetchSlides = async()=>{
 
        try{
 
         const resp =  await api.get("/api/admin/slider/getsliders");
         setSlideItems(resp);
+        setLoading(false);
        }catch(ex){
 
+        setLoading(false);
        }
     
        
@@ -18,7 +38,12 @@ export default function Slider(){
     
     useEffect(() => {
         fetchSlides();
-      },[slideItems]);
+      },[]);
+
+
+      return(<>
+     {!slideItems &&<p>Hey</p>}
+      </>);
 
 }
 
